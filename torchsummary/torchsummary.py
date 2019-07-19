383da6b0eb8fc2c2ce58884a0deb8edebb86c29c
Reza Baharani <mbaharan@uncc.edu>
2019-07-19 20:27:15 +0000

    Saving the rule file for dynamic Fixed-Point Arith

--- torchsummary/torchsummary.py
+++ torchsummary/torchsummary.py
@@ -8,9 +8,28 @@ import numpy as np
 from models.imagenet.mobilenetv2 import mobilenetv2, InvertedResidual
 
 from os import path
+import math
 
 
-def summary(model, input_size, batch_size=-1, device="cuda", q_bit=12, save_data=False, file_path='./'):
+def save_q_bit_rule(model, bit_list, file_path, bit_width):
+    w_param = [p for p in model.state_dict() if (
+        'weight' in p and not 'classifier' in p)]
+    b_param = [p for p in model.state_dict() if (
+        'bias' in p and not 'classifier' in p)]
+    assert len(w_param) == len(bit_list['int_w_weight'])
+    assert len(b_param) == len(bit_list['int_w_bias'])
+    file_name = path.join(file_path, 'rule.txt')
+    with open(file_name, 'w') as handler:
+        for name, i in zip(w_param, bit_list['int_w_weight']):
+            handler.write(
+                '.*\\.{} fixed_point {} {}\n'.format(name, bit_width, i))
+        for name, i in zip(b_param, bit_list['int_w_bias']):
+            handler.write(
+                '.*\\.{} fixed_point {} {}\n'.format(name, bit_width, i))
+    print('Rule has been saved in {}.'.format(file_name))
+
+
+def summary(model, input_size, batch_size=-1, device="cuda", q_bit=16, save_data=False, file_path='./'):
 
     def register_hook(module):
 
@@ -58,18 +77,40 @@ def summary(model, input_size, batch_size=-1, device="cuda", q_bit=12, save_data
                     np.save(
                         path.join(file_path[0], m_key+'_bias'), module.bias.detach().cpu())
 
+            summary[m_key] = OrderedDict()
+
             if isinstance(module, nn.Conv2d):
-                max_min_weight[0] = max(
-                    max_min_weight[0], module.weight.detach().cpu().max())
-                
-                max_min_weight[1] = min(
-                    max_min_weight[1], module.weight.detach().cpu().min())
-                
-                max_min_bias[0] = max(
-                    max_min_bias[0], module.weight.detach().cpu().max())
-                
-                max_min_bias[1] = min(
-                    max_min_bias[1], module.bias.detach().cpu().min())
+                w = module.weight.detach().cpu()
+                b = module.bias.detach().cpu()
+                min_v = w.min()
+                max_v = w.max()
+                b_max_v = b.max()
+                b_min_v = b.min()
+
+                max_min_weight[0] = max(max_min_weight[0], max_v)
+                max_min_weight[1] = min(max_min_weight[1], min_v)
+                max_min_bias[0] = max(max_min_bias[0], b_max_v)
+                max_min_bias[1] = min(max_min_bias[1], b_min_v)
+
+                summary[m_key]["weight_range"] = [min_v.item(), max_v.item()]
+                summary[m_key]["bias_range"] = [b_min_v.item(), b_max_v.item()]
+                summary[m_key]["spr"] = (1-(float(np.count_nonzero(w)) /
+                                            np.prod(w.size())))*100
+
+                int_bit = math.ceil(math.log2(max(abs(min_v), abs(max_v)))) + 1
+                b_int_bit = math.ceil(
+                    math.log2(max(abs(b_min_v), abs(b_max_v)))) + 1
+
+                if min_v < 0 and int_bit == 0:
+                    int_bit = 1
+                if b_min_v < 0 and b_int_bit == 0:
+                    b_int_bit = 1
+
+                summary[m_key]["int_bit_size"] = int_bit
+                summary[m_key]["b_int_bit_size"] = b_int_bit
+
+                int_bit_val['int_w_weight'].append(int_bit)
+                int_bit_val['int_w_bias'].append(b_int_bit)
 
             input_buf_size = np.prod(input[0].size())
             output_buf_size = np.prod(output.size())
@@ -88,7 +129,6 @@ def summary(model, input_size, batch_size=-1, device="cuda", q_bit=12, save_data
                             max_buf_size_value[i], output.size()[i+1])
 
             # m_key = "%s-%i" % (class_name, module_idx + 1)
-            summary[m_key] = OrderedDict()
             summary[m_key]["input_shape"] = list(input[0].size())
             summary[m_key]["max_input_shape"] = list(input[0].size())
             summary[m_key]["input_shape"][0] = batch_size
@@ -150,6 +190,9 @@ def summary(model, input_size, batch_size=-1, device="cuda", q_bit=12, save_data
     block = list([-1])
     max_min_weight = list([float('-Inf'), float('+Inf')])
     max_min_bias = list([float('-Inf'), float('+Inf')])
+    int_bit_val = OrderedDict()
+    int_bit_val['int_w_bias'] = list()
+    int_bit_val['int_w_weight'] = list()
 
     hooks = []
 
@@ -165,20 +208,40 @@ def summary(model, input_size, batch_size=-1, device="cuda", q_bit=12, save_data
         h.remove()
 
     print("----------------------------------------------------------------")
-    line_new = "{:>25}  {:>25} {:>15}".format(
-        "Layer (type)", "Output Shape", "Param #")
+    line_new = "{:>25}  {:>25} {:>25} {:>15} {:>15} {:>25} {:>15} {:>15}".format(
+        "Layer (type)", "Output Shape", "Weight range", "Int. bit", "Sparsity", "Bias range", "Bias Int. bit", "Param #")
     print(line_new)
-    print("================================================================")
+    print("===========================================================================================================================")
     total_params = 0
     total_output = 0
     trainable_params = 0
     for layer in summary:
-        # input_shape, output_shape, trainable, nb_params
-        line_new = "{:>25}  {:>25} {:>15}".format(
-            layer,
-            str(summary[layer]["output_shape"]),
-            "{0:,}".format(summary[layer]["nb_params"]),
-        )
+        if 'conv' in layer.lower():
+            # input_shape, output_shape, w range, int bit, trainable, nb_params
+            line_new = "{:>25}  {:>25} {:>25} {:>15} {:>15} {:>25} {:>15} {:>15}".format(
+                layer,
+                str(summary[layer]["output_shape"]),
+                "{:2f}, {:2f}".format(
+                    summary[layer]["weight_range"][0], summary[layer]["weight_range"][1]),
+                str(summary[layer]["int_bit_size"]),
+                "{:2f}".format(summary[layer]["spr"]),
+                "{:2f}, {:2f}".format(
+                    summary[layer]["bias_range"][0], summary[layer]["bias_range"][1]),
+                str(summary[layer]["b_int_bit_size"]),
+                "{0:,}".format(summary[layer]["nb_params"]),
+            )
+        else:
+            line_new = "{:>25}  {:>25} {:>25} {:>15} {:>15} {:>25} {:>15} {:>15}".format(
+                layer,
+                str(summary[layer]["output_shape"]),
+                # "{:2f}, {:2f}".format(summary[layer]["weight_range"][0], summary[layer]["weight_range"][1]),
+                "-----",
+                "-----",  # str(summary[layer]["int_bit_size"]),
+                "-----",
+                "-----",
+                "-----",
+                "{0:,}".format(summary[layer]["nb_params"]),
+            )
         total_params += summary[layer]["nb_params"]
         total_output += np.prod(summary[layer]["output_shape"])
         if "trainable" in summary[layer]:
@@ -209,4 +272,6 @@ def summary(model, input_size, batch_size=-1, device="cuda", q_bit=12, save_data
     print("Weight range: %s" % (str(max_min_weight)))
     print("Bias range: %s" % (str(max_min_bias)))
     print("----------------------------------------------------------------")
+    print("Saving rule for bit quantization...")
+    save_q_bit_rule(model, int_bit_val, file_path[0], q_bit)
     # return summary
